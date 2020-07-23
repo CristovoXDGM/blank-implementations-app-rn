5c03b9ff7de64013ba285a74d63e76fe4f4ac1ef
cristovoxdgm <cristovoxdgm@gmail.com>
2020-07-23 02:26:04 -0300

    finishing the last class abou http request with axios

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 import { api } from './src/services/api';
 import { FlatList } from 'react-native-gesture-handler';
 import Filmes from './src/Filmes';
 
 
 export default function App() {
-  const responsedata = [];
+  const [loading, setLoading] = useState(true);
   const [filmes, setFilmes] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const result = await api.get('r-api/?api=filmes');
       setFilmes(result.data);
+      setLoading(false);
     }
     fetchData();
 
     console.log(filmes);
   }, [])
 
-  return (
-    <View style={styles.container}>
-      <FlatList data={filmes} keyExtractor={item => item.id.toString()} renderItem={(item) => <Filmes prop={item} />} />
-      <StatusBar style="auto" />
-    </View>
-  );
+  if (loading) {
+    return (
+      <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }} >
+        <ActivityIndicator color="#09af" size={40} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={filmes}
+          keyExtractor={item => item.id.toString()}
+          renderItem={(item) => <Filmes prop={item} />} />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- src/Filmes/index.js
+++ src/Filmes/index.js
@@ -1,15 +1,71 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 // import { Container } from './styles';
 
 function Filmes({ prop }) {
-    console.log(prop);
+    console.log(prop.item.nome);
+
     return (
         <View>
-            <Text style={{ fontSize: 40, marginTop: 20 }}>...</Text>
+            <View style={styles.card}>
+                <Text style={styles.titulo}>{prop.item.nome}</Text>
+                <Image
+                    source={{ uri: prop.item.foto }}
+                    style={styles.capa}
+                />
+                <View style={styles.areaBotao}>
+                    <TouchableOpacity style={styles.botao} onPress={() => alert(prop.item.nome)} >
+                        <Text style={styles.botaoTexto} >LEIA MAIS</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    card: {
+        shadowColor: "#000",
+        backgroundColor: "#fff",
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        margin: 15,
+        shadowRadius: 5,
+        borderRadius: 5,
+        elevation: 3,
+        zIndex: 1,
+    },
+    titulo: {
+        fontSize: 18,
+        padding: 15,
+
+    },
+    capa: {
+        height: 250,
+        width: Dimensions.get('window').width / 1.1
+    },
+    botao: {
+        width: 100,
+        backgroundColor: "#09a6ff",
+        opacity: 1,
+        padding: 8,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5
+    },
+    botaoTexto: {
+        textAlign: "center",
+        color: "#fff"
+    },
+    areaBotao: {
+        alignItems: "flex-end",
+        marginTop: -40,
+        zIndex: 9
+    }
+});
 
 export default Filmes;
