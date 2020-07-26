efabba154b8f4f17ce82583d659763914e9e683a
cristovoxdgm <cristovoxdgm@gmail.com>
2020-07-26 16:46:42 -0300

    learning about animation interpolation

--- App.js
+++ App.js
@@ -1,82 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, Text, Animated } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
-
-
+import React, { useRef, useEffect } from 'react';
+import { StyleSheet, View, Animated } from 'react-native';
 
 export default function App() {
 
-  const [larguraAnimada, setLarguraAnimada] = useState(new Animated.Value(150));
-  const alturaAnim = useRef(new Animated.Value(50)).current;
-  const opacityAnim = useRef(new Animated.Value(0)).current;
-
-
-  const carregarGrafico = () => {
-
-
-    Animated.sequence(
-      [
-
-        Animated.timing(opacityAnim, {
-          toValue: 100,
-          duration: 2500
-        }),
-        Animated.timing(alturaAnim, {
-          toValue: 500,
-          duration: 2500
-        }),
-      ]
-    ).start()
-
-  }
-
+  const larAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
+    Animated.timing(larAnim, {
+      toValue: 100,
+      duration: 4000
+    }).start()
+  }, []);
 
-
-
-  }, [])
-
+  let porcentafeInter = larAnim.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['0%', '100%']
+  })
 
   return (
     <View style={styles.container}>
 
-      <View style={{
-        backgroundColor: "#4169e1",
-        height: 80,
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: "center",
-        flexDirection: 'row'
-      }}>
+      <Animated.View style={{ backgroundColor: "#4169E1", width: porcentafeInter, height: 25 }}>
 
-        <TouchableOpacity onPress={carregarGrafico} >
-          <Text style={{ fontSize: 25, color: "#fff" }}>
-            Gerar Gráfico
-            </Text>
-        </TouchableOpacity>
-
-      </View>
-
-
-
-      <View style={{ flex: 1, flexDirection: "column", justifyContent: "flex-end", alignItems: "center" }}>
-        <Text>Vendas</Text>
-        <Animated.View style={{ height: alturaAnim, opacity: opacityAnim, width: 150, backgroundColor: "#4169e1", justifyContent: "center" }}>
-          <Text
-            style={{
-              fontSize: 22,
-              color: "#fff",
-              textAlign: "center"
-            }} >
-            R$150,00
-          </Text>
-        </Animated.View>
-      </View>
-
-
+      </Animated.View>
 
       <StatusBar style="auto" />
     </View>
@@ -88,6 +35,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: "center",
 
   }
 });
